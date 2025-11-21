1d63537caf1bacb04c07e6323b3b9b92d5558c3a
nicolasgrajaleshoyos <nicolasgrajaleshoyos@gmail.com>
2025-11-21 13:35:12 -0500

    subiendo dia viernes

--- database/migrations/2025_11_20_213842_create_posts_table.php
+++ database/migrations/2025_11_20_213842_create_posts_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employers', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
-            $table->string('companyEmail');
+            $table->string('title');
+            $table->text('body');
             
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employers');
+        Schema::dropIfExists('posts');
     }
 };
